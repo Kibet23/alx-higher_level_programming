4ce5d2da867d3ffe6209efa1574becaadddcc0e1
Kibet23 <kibettuwei117@gmail.com>
2024-01-16 13:31:26 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for num in range(10):
     for num2 in range(num + 1, 10):
         if num != num2:
             comb = "{:d}{:d}".format(num, num2)
-            sep = ", " if num < 8 or num2 <  9 else "\n"
+            sep = ", " if num < 8 or num2 < 9 else "\n"
             print(comb, end=sep)
