8261f38065e05ad73d64e6c90db55063c31510eb
Kibet23 <kibettuwei117@gmail.com>
2024-01-16 16:23:02 -0800

    10-add.py

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    adds two integers and returns the result
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        int: sum of a and b
+    """
+    return a+b
