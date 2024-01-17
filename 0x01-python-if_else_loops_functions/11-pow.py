dd341b5b24bee8de6534b2cb75cb54f5f016c7df
Kibet23 <kibettuwei117@gmail.com>
2024-01-16 16:35:06 -0800

    11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """
+    computes a to the power of b and returns the value
+
+    Args:
+        a: the base
+        b: argument
+    Return:
+        int: the result of a raised to the power of b
+    """
+    return a ** b
