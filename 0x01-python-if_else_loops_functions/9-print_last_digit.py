fbdc0ccc93160061a2e8f20d96b14a681a406cb0
Kibet23 <kibettuwei117@gmail.com>
2024-01-16 16:07:42 -0800

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    """
+    print the last digit of a given number
+
+    Args:
+        number: input number
+
+    Return:
+        NULL
+    """
+    last_digit = abs(number) % 10
+    print(last_digit)
+    return last_digit
