babe05f9ea7457a66d393ea9ec5cd6bc9e7d6868
Kibet23 <kibettuwei117@gmail.com>
2024-02-20 15:42:20 -0800

    100-main.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines search and update"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of a text to a file, each line
+    containing a specific string"""
+
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    with open(filename, "w") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string + "\n")
