c3523e1879495769d822c1983d2a0c914cf6ab0d
Kibet23 <kibettuwei117@gmail.com>
2024-02-20 11:25:41 -0800

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defines Load, add, save"""
+
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    """get existing items from the file"""
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+
+    """Add the new items from command line args"""
+    items.extend(sys.argv[1:])
+
+    """Save updated items list to the file"""
+    save_to_json_file(items, "add_item.json")
