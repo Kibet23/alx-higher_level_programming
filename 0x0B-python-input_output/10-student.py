f38a63ff64ebc2686f960ff72b085ef9dbedc72f
Kibet23 <kibettuwei117@gmail.com>
2024-02-20 13:28:19 -0800

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes student name with first_name, last_name andd age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retreives a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
