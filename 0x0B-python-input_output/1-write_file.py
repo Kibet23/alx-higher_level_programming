f6293f92d0b3ff9e497e775ca89d881cc7704ff1
Kibet23 <kibettuwei117@gmail.com>
2024-02-20 07:26:28 -0800

    writes a string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines a write function"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)
+    Args:
+        filename - name of the file to be written
+        text - text to be written to the file
+    Return:
+        number of characters written"""
+
+
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
