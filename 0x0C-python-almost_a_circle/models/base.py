d73681f0ddc2f49ffa6ed97bc1b7ee5f21da4b9c
Kibet23 <kibettuwei117@gmail.com>
2024-02-27 06:13:13 -0800

    14-main

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,11 +17,12 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON string representation of list_dictionaries
         Args:
             list_dictionaries (list): list of dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) == []:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
