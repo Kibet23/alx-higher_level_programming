30a283ca2f12e1b8b0d12ac23410b6b7e2a58909
Kibet23 <kibettuwei117@gmail.com>
2024-02-27 00:13:14 -0800

    14-main

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """defines the Base class"""
 
 
+import json
+
 class Base:
     """defines the class Base"""
 
@@ -15,3 +17,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
